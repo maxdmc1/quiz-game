639702faffe1aa69be976529ecc6e3207098c1d0
Max McCord <maxdmccord@gmail.com>
2019-11-19 22:52:30 -0600

    timer and score is working - need to decrement time if incorrect answer is given

--- script.js
+++ script.js
@@ -41,29 +41,28 @@ let question = 0
 const startContainerEl = document.getElementById("start-container")
 const quizSpaceEl = document.getElementById("quiz-space");
 const resultSpaceEl = document.getElementById("result-space");
+const clockEl = document.getElementById("clock");
+const timerSpaceEl = document.getElementById("timer-space");
 
 let correctAnswer = 0
 let wrongAnswer = 0
-
+let score = 0
 
 function startQuiz() {
     startContainerEl.setAttribute("class", "container d-none")
     quizSpaceEl.setAttribute("class", "container")
+    timerSpaceEl.setAttribute("class", "container")
+    console.log("we're starting")
 
-
+    startTimer()
     newQuestion(question)
 }
-// Tutor assisted with this portion - this is jQuery
+
 function newQuestion(q) {
     quizSpaceEl.innerHTML = "";
-    // quizSpaceEl.innerHTML = `<p>${questions[question].title} </p>
-    // <button value ="${questions[question].choices[0]}" onclick="compareAnswer(this)" >${questions[question].choices[0]}</button>
-    // <button value ="${questions[question].choices[1]}" onclick="compareAnswer(this)" >${questions[question].choices[1]}</button>
-    // <button value ="${questions[question].choices[2]}" onclick="compareAnswer(this)" >${questions[question].choices[2]}</button>
-    // <button value ="${questions[question].choices[3]}" onclick="compareAnswer(this)" >${questions[question].choices[3]}</button>`
     const quizQuestionEl = document.createElement("div");
     quizSpaceEl.append(quizQuestionEl);
-    quizQuestionEl.setAttribute("class", "row m-5")
+    quizQuestionEl.setAttribute("class", "row mt-5 mb-5")
     // for (let i = 0; i < questions.length; i++) {
     quizQuestionEl.innerHTML = questions[q].title;
 
@@ -73,18 +72,56 @@ function newQuestion(q) {
         buttonEl.innerHTML = questions[q].choices[j];
         buttonEl.addEventListener("click", compareAnswer);
         buttonEl.setAttribute("value", questions[q].choices[j]);
+        buttonEl.setAttribute("class", "m-3")
     }
 
-
-
 }
+
+
+const defaultTime = questions.length * 30;
+let time = 0
+let myInterval = null
+console.log(defaultTime);
+
+function startTimer() {
+    time = defaultTime
+
+        myInterval = setInterval(function () {
+            time = time - 1;
+            console.log(time);
+            localStorage.setItem("time", time);
+
+            let minutes = Math.floor(time / 60) % 60;
+            let seconds = time % 60;
+
+            if (minutes < 10) {
+                minutes = "0" + minutes
+            }
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            clockEl.innerHTML = minutes + ":" + seconds;
+
+            if (time <= 0) {
+                clearInterval(myInterval);
+            }
+        }, 1000)
+
+    };
+
+
 
 function compareAnswer(event) {
     let userInput = event.target.getAttribute("value");
     console.log(userInput, this);
     if (userInput === questions[question].answer) {
-        correctAnswer++
-        
+        // correctAnswer++
+        if (score === 0){
+            score = time;
+        }else{
+            score = score + time
+        }
+
     } else (
         wrongAnswer++
     )
@@ -99,59 +136,18 @@ function compareAnswer(event) {
 function displayResults() {
     quizSpaceEl.innerHTML = "";
     quizSpaceEl.setAttribute("class", "d-none");
+    timerSpaceEl.setAttribute("class", "d-none");
     resultSpaceEl.setAttribute("class", "container mt-5 border border-dark");
     // display totals 
-    resultSpaceEl.innerHTML = "Your Score is: " + correctAnswer
+    resultSpaceEl.innerHTML = "Your Score is: " + score
 
 
     // display button to restart the game
     // 
 }
-
+// window.timer = timer
 // create timer
 
-// // this was taken from in class activity - does not work
-// const defaultTime = 300;
-// const clockEl = document.getElementById("clock");
-// const userEl = document.getElementById("user-time-in-seconds");
-// let savedValue = localStorage.getItem("time");
-// let time = 0;
-// if (savedValue) {
-//   time = savedValue;
-// } else {
-//   time = defaultTime;
-// }
-// let myInterval = null;
-// function startTimer() {
-//     if (myInterval) {
-//         clearInterval(myInterval);
-//         myInterval = null;
-//     }
-//     myInterval = setInterval(function () {
-//         time = time - 1;
-//         localStorage.setItem("time", time);
-
-//         let hours = Math.floor(time / 60 / 60);
-//         let minutes = Math.floor(time / 60) % 60;
-//         let seconds = time % 60;
-
-//         if (hours < 10) {
-//             hours = "0" + hours;
-//         }
-//         if (minutes < 10) {
-//             minutes = "0" + minutes;
-//         }
-//         if (seconds < 10) {
-//             seconds = "0" + seconds;
-//         }
-
-//         clockEl.innerHTML = hours + ":" + minutes + ":" + seconds;
-
-//         if (time <= 0) {
-//             clearInterval(myInterval);
-//         }
-//     }, 1000);
-// }
 
 
 
